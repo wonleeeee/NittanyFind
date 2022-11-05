42b784781650bbc37f3e15c183a24e527d30391a
akdrh3 <im7738@naver.com>
2022-11-05 16:53:03 -0400

    file add

---
+++ lost/router.js
@@ -0,0 +1,23 @@
+var lost = require("./found_model");
+var router = require("express").Router();
+
+// function getBook(req, res) {
+//     //console.log("Test")
+//     fileBoard.getBook((err,result) => {
+//         if (err){
+//             console.log(err);
+//         }
+//         else {
+//             fileBoard.getPageNum((err,result2) => {
+//                 if (err) {
+//                     console.log (err);
+//                 }
+//                 res.json({
+//                     pageNum : Math.ceil(result2[0]["pageNum"]/10),
+//                     boardData: result}
+//                 );
+//             });
+//         }
+//     });
+// }
+function findMatch
